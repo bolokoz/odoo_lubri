1bb52d5b01e120ac8132a0d261a996c8dbbb2b31
bolokoz <yurifbeck@gmail.com>
2024-01-19 14:41:30 -0400

    main module now depends on many modules

--- custom_addons/lubri2024/__manifest__.py
+++ custom_addons/lubri2024/__manifest__.py
@@ -3,15 +3,15 @@
     'name': "lubri2024",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Modulos para a empresa Lubrimaster
+        """,
 
     'description': """
-        Long description of module's purpose
+        Instala vários módulos designados para o funcionamento da empresa!!!
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Yuri Becker",
+    'website': "https://www.yuribecker.com.br",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -20,16 +20,39 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': [
+        'base',
+        'sale_management',
+        'stock',
+        'purchase',
+        'knowledge',
+        'hr_expense',
+        'project',
+        'website_sale',
+        'planning',
+        'fleet',
+        'contacts',
+        'note',
+        'calendar',
+        'maintenance',
+        'hr',
+        'delivery',
+        'stock_picking_batch',
+        'purchase_requisition',
+        'board',
+        'knowledge',
+        'sale_margin'
+    ],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'data/config.xml'
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    # 'demo': [
+        # 'demo/demo.xml',
+    # ],
 }
